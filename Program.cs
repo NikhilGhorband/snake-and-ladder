8c963bc38dbaf0151997dc23ce4087888c4a783e
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-13 00:06:40 +0530

    Dice Rolled

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DieRolled
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int result = random.Next(1, 7); // Generates a random number between 1 and 6
+
+            Console.WriteLine("The die rolled: " + result);
+        }
+    }
+}
