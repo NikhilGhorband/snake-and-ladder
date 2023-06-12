d999f7e940837c1491ea0b3051f2247d37b9d654
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-13 00:09:56 +0530

    Dice Rollled Number

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SnakeandLadder_UC1
+{
+    public class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine(" Welcome to Snake and Ladder\n ");
+
+            int position = 0;
+            int playerone;
+            playerone = position;
+            Console.WriteLine("Playerone position is:" + playerone +"\n");
+
+
+
+        }
+    }
+}
